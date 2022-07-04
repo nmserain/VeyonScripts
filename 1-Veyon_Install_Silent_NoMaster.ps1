05a42dfa5836413a73d1fc9cf196eb407d72a147
Nicolas SERAIN <n.serain@gmail.com>
2022-07-04 15:53:07 -0400

    Delete 1-Veyon_Install_Silent_NoMaster.ps1
    remove restriction code

--- 1-Veyon_Install_Silent_NoMaster.ps1
+++
@@ -1,5 +0,0 @@
-$setupPath = ".\veyon-4.7.3.0-win64-setup.exe"
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-Invoke-expression "$setupPath /S /nomaster"
-echo "Done"
-Start-Sleep -Seconds 2
